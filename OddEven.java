760afbe27cd32ee1de2e9731e3beb477cb9d5443
Inchara-N-Shankar <vvce22ise0008@vvce.ac.in>
2025-03-24 12:55:36 +0530

    Create OddEven.java

---
+++ OddEven.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        sc.close();
+
+        System.out.println(n % 2 == 0 ? "Even" : "Odd");
+    }
+}
